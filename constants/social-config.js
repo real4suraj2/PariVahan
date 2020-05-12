4c11b7daffa19ad102a7ae7abcb3548b89e21dd4
Suraj <48567316+real4suraj2@users.noreply.github.com>
2020-05-12 12:58:26 +0530

    Update social-config.js

--- constants/social-config.js
+++ constants/social-config.js
@@ -1,9 +1,9 @@
-const FACEBOOK_APP_ID = "834072043737963";
+const FACEBOOK_APP_ID = "";
 const GOOGLE_IOS_ID = "";
 const GOOGLE_ANDROID_ID =
-	"746591055474-uva2rfpk4cje6pe57ottjpcnirfaifab.apps.googleusercontent.com"; //DEV
+	""; //DEV
 const GOOGLE_ANDROID_ID_STANDALONE =
-	"746591055474-vcnhs3ca5dndiolrkdgtcr3lsb44f6gs.apps.googleusercontent.com"; //PROD
+	""; //PROD
 const API_URL = "http://5e08ac18434a370014168b98.mockapi.io/api/v1"; //FUTURE IMPLEMENTATION
 
 export {
