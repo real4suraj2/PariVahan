b99e2d5445b39e41d276483bd696802fe034aa30
Suraj <48567316+real4suraj2@users.noreply.github.com>
2020-05-12 12:59:33 +0530

    Update firebase-config.js

--- constants/firebase-config.js
+++ constants/firebase-config.js
@@ -1,14 +1,14 @@
 import * as firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBK5IE7QijEp3tZKSCYKb0tDpK5NrwjK_g",
-  authDomain: "parivahan-1f4eb.firebaseapp.com",
-  databaseURL: "https://parivahan-1f4eb.firebaseio.com",
-  projectId: "parivahan-1f4eb",
-  storageBucket: "parivahan-1f4eb.appspot.com",
-  messagingSenderId: "567101954343",
-  appId: "1:567101954343:web:e58ab32904b657a0257102",
-  measurementId: "G-JXPNN6RKBH",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
